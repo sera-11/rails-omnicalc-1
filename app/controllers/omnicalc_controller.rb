090eb52d2bf084cef6aaf7c89d1a757cd2712ed4
Sera Oraha <72953661+sera-11@users.noreply.github.com>
2024-02-07 16:42:32 +0000

    completed square form and homepage

--- app/controllers/omnicalc_controller.rb
+++ app/controllers/omnicalc_controller.rb
@@ -1,1 +1,11 @@
 #app/controllers/omnicalc_controller.rb
+class OmnicalcController < ApplicationController
+
+  def square_form
+  
+    render({:template => "omnicalc_templates/square_form"})
+  end
+
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 #config/routes.rb
 Rails.application.routes.draw do
-  
+
+  get("/", { :controller => "omnicalc", :action => "square_form" })
+
+  get("/square/new", { :controller => "omnicalc", :action => })
 end
